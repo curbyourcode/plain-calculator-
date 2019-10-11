90cfc8e71f9dfd686333e342025b8e8f0309d340
Rouzbeh Majlessi <rouz@Rouzs-MacBook-Pro.local>
2019-10-10 22:52:17 -0700

    Add material toolbar with home and dial icons to app comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import { MatToolbarModule,
+         MatMenuModule,
+         MatIconModule,
+         MatCardModule,
+         MatButtonModule,
+         MatTableModule,
+         MatDividerModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDividerModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
